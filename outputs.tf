16b1a39f4131a95882c5488490d8965ee00f753c
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:44:51 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "guardduty_organization_configurations_auto_enable_organization_members" 
 }
 output "guardduty_organization_configurations_datasources" {
   description = "Map of datasources values across all guardduty_organization_configurations, keyed the same as var.guardduty_organization_configurations"
-  value       = { for k, v in aws_guardduty_organization_configuration.guardduty_organization_configurations : k => v.datasources if v.datasources != null && length(v.datasources) > 0 }
+  value       = { for k, v in aws_guardduty_organization_configuration.guardduty_organization_configurations : k => one(v.datasources) if v.datasources != null && length(v.datasources) > 0 }
 }
 output "guardduty_organization_configurations_detector_id" {
   description = "Map of detector_id values across all guardduty_organization_configurations, keyed the same as var.guardduty_organization_configurations"
